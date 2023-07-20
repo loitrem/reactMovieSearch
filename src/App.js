0afac46cbb4484c77f55ac20bebd5cf2ba912cc1
loitrem <ski_fix@hotmail.com>
2023-07-20 13:13:53 -0400

    added components and api function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import Form from './components/Form';
+import MovieDisplay from './components/MovieDisplay';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  //api key
+  const apiKey = "ca69f089";
+
+  //movie state
+  const [movie, setMovie] = useState(null);
+
+  //Function to getMovies
+  const getMovie = async (searchTerm) => {
+    // make fetch request and store response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a javascript object
+    const data = await response.json();
+    //set the Movie state to the movie
+    setMovie(data);
+  };
+
   return (
     <div className="App">
-
+      <Form/>
+      <MovieDisplay/>
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div>
+
+            <form>
+                <input className='inputText' type="text"/>
+                <input className='submitButton' type="submit" value="submit"/>
+            </form>
+
+
+        </div>
+    )
+}
+
+export default Form
